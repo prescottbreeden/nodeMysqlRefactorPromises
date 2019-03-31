9cbc054cbc055035f52b7eeadae9e047583e1702
prescottbreeden <prescott.breeden@gmail.com>
2019-03-31 11:08:14 -0700

    pseudo coded what original controller looked like

--- oldController.js
+++ oldController.js
@@ -1,19 +1,40 @@
 const logger = require('../_helpers/logger');
 const {database:connect} = require('../../config')['development'];
-const db = require('mysql').createConnection(connect);
-const {
-  queryNewAnswer,
-  queryGetAnswer,
-  queryUpdateAnswer,
-  queryAcceptAnswer,
-  queryDeleteAnswer
-} = require('../dbQueries/answer.queries');
+const mysql = require('mysql');
+const db = mysql.createConnection(connect);
 
 module.exports = {
 
   addAnswer: (req, res) => {
     const DATA = req.body;
-    db.query(queryNewAnswer, [DATA], (error, results) => {
+    db.query(
+      `SELECT u.user_id,
+              f.faction_name,
+              u.first_name,
+              u.last_name,
+              u.email,
+              u.password,
+              u.admin,
+              u.profile_img,
+              u.konami_unlock,
+              u.user_created,
+              u.user_updated,
+              COUNT(DISTINCT b.bug_id) AS bugs,
+              COUNT(DISTINCT a.answer_id) AS answers,
+              COUNT(DISTINCT fav.favorite_id) AS favorites
+         FROM users AS u
+         JOIN factions AS f
+           ON f.faction_id = u.faction_id
+    LEFT JOIN bugs AS b
+           ON b.posted_by = user_id
+    LEFT JOIN answers AS a
+           ON a.answered_by = user_id
+    LEFT JOIN favorites AS fav
+           ON fav.user_id = u.user_id
+        WHERE u.email = ?
+     GROUP BY u.user_id`, [DATA], 
+     
+     function (error, results) {
       if (error) {
         logger.log('warn', `bugs.addAnswer(): ${error}`);
         res.json(error);
@@ -26,7 +47,33 @@ module.exports = {
 
   getAnswer: (req, res) => {
     const {answer_id:ID} = req.params;
-    db.query(queryGetAnswer , [ID], (error, results) => {
+    db.query(
+      `SELECT u.user_id,
+              f.faction_name,
+              u.first_name,
+              u.last_name,
+              u.email,
+              u.password,
+              u.admin,
+              u.profile_img,
+              u.konami_unlock,
+              u.user_created,
+              u.user_updated,
+              COUNT(DISTINCT b.bug_id) AS bugs,
+              COUNT(DISTINCT a.answer_id) AS answers,
+              COUNT(DISTINCT fav.favorite_id) AS favorites
+         FROM users AS u
+         JOIN factions AS f
+           ON f.faction_id = u.faction_id
+    LEFT JOIN bugs AS b
+           ON b.posted_by = user_id
+    LEFT JOIN answers AS a
+           ON a.answered_by = user_id
+    LEFT JOIN favorites AS fav
+           ON fav.user_id = u.user_id
+        WHERE u.user_id = ?
+     GROUP BY u.user_id`, [ID], 
+     function(error, results) {
       if (error) {
         logger.log('warn', `bugs.getAnswer(): ${error}`);
         res.json(error);
@@ -39,7 +86,34 @@ module.exports = {
   updateAnswer: (req, res) => {
     const {answer_id:ID} = req.params;
     const {answer_content:answer} = req.body;
-    db.query(queryUpdateAnswer , [answer, ID], (error, results) => {
+    db.query( 
+      
+      `SELECT u.user_id,
+              f.faction_name,
+              u.first_name,
+              u.last_name,
+              u.email,
+              u.password,
+              u.admin,
+              u.profile_img,
+              u.konami_unlock,
+              u.user_created,
+              u.user_updated,
+              COUNT(DISTINCT b.bug_id) AS bugs,
+              COUNT(DISTINCT a.answer_id) AS answers,
+              COUNT(DISTINCT fav.favorite_id) AS favorites
+         FROM users AS u
+         JOIN factions AS f
+           ON f.faction_id = u.faction_id
+    LEFT JOIN bugs AS b
+           ON b.posted_by = user_id
+    LEFT JOIN answers AS a
+           ON a.answered_by = user_id
+    LEFT JOIN favorites AS fav
+           ON fav.user_id = u.user_id
+        WHERE u.user_id = ?
+     GROUP BY u.user_id`, [answer, ID], 
+     function(error, results) {
       if (error) {
         logger.log('warn', `bug.update(): ${error}`);
         res.json(error);
@@ -53,7 +127,34 @@ module.exports = {
   acceptAnswer: (req, res) => {
     const {answer_id:ID} = req.params;
     const {accepted} = req.body;
-    db.query(queryAcceptAnswer, [accepted, ID], (error, results) => {
+    db.query(
+      `SELECT u.user_id,
+              f.faction_name,
+              u.first_name,
+              u.last_name,
+              u.email,
+              u.password,
+              u.admin,
+              u.profile_img,
+              u.konami_unlock,
+              u.user_created,
+              u.user_updated,
+              COUNT(DISTINCT b.bug_id) AS bugs,
+              COUNT(DISTINCT a.answer_id) AS answers,
+              COUNT(DISTINCT fav.favorite_id) AS favorites
+         FROM users AS u
+         JOIN factions AS f
+           ON f.faction_id = u.faction_id
+    LEFT JOIN bugs AS b
+           ON b.posted_by = user_id
+    LEFT JOIN answers AS a
+           ON a.answered_by = user_id
+    LEFT JOIN favorites AS fav
+           ON fav.user_id = u.user_id
+        WHERE u.user_id = ?
+     GROUP BY u.user_id`, [accepted, ID], 
+
+     function (error, results) {
       if (error) {
         logger.log('warn', `bug.update(): ${error}`);
         res.json(error);
@@ -62,11 +163,6 @@ module.exports = {
         res.json(results);
       }
     });
-
   },
-
-  deleteAnswer: (req, res) => {
-    console.log(queryDeleteAnswer);
-  }
 
 };
